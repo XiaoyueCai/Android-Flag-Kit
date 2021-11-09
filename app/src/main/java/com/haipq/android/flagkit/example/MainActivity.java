a43ac0728eda9385c20c2d7d2383ee3b11819d66
XiaoyueCai <35023208+XiaoyueCai@users.noreply.github.com>
2021-11-09 17:50:17 +0800

    refactor: Migrate to AndroidX

--- app/src/main/java/com/haipq/android/flagkit/example/MainActivity.java
+++ app/src/main/java/com/haipq/android/flagkit/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.haipq.android.flagkit.example;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import com.haipq.android.flagkit.FlagImageView;
